a7a3e0a179bc50f00fb08d803becd866de1d5d51
kitochio <mtakaba84@gmail.com>
2024-06-12 17:48:30 +0900

    コマンドと引数

--- GamePlugin/src/main/java/kitochio/gameplugin/command/MatchingGameCommand.java
+++ GamePlugin/src/main/java/kitochio/gameplugin/command/MatchingGameCommand.java
@@ -8,7 +8,37 @@ public class MatchingGameCommand extends BaseCommand {
 
   @Override
   protected boolean onExecutePlayerCommand(Player player, Command command, String s, String[] strings) {
-    player.sendMessage("Hello World!");
+    if (strings.length >= 1) {
+      //第一引数にendが入力されたらゲーム終了、rankが入力されたらランキングを表示
+      switch (strings[0]) {
+        case "end" -> {
+          player.sendMessage("ゲームを終了します");
+          return false;
+        }
+        case "rank" -> {
+          player.sendMessage("ランキングを表示します");
+          return false;
+        }
+        case "Level" -> {
+          switch (strings[1]) {
+            case "1" -> {
+              player.sendMessage("難易度をLEVEL１に設定します");
+            }
+            case "2" -> {
+              player.sendMessage("難易度をLEVEL2に設定します");
+            }
+            case "3" -> {
+              player.sendMessage("難易度をLEVEL3に設定します");
+            }
+          }
+        }
+        default -> {
+          player.sendMessage("設定のない引数が入力されました\nend:ゲームを終了　rank:ランキングを表示");
+          return false;
+        }
+      }
+    }
+    player.sendMessage("ゲームを開始します");
     return true;
   }
 
